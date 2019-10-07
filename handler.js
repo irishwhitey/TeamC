7d8394d896e18cd9055a482d0f08b96d6b144fe3
Martin White <martin.white@codecomputerlove.com>
2019-10-07 14:07:17 +0100

    made response more realisitic

--- handler.js
+++ handler.js
@@ -1,12 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.calculate = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        result: '6'        
       },
       null,
       2
