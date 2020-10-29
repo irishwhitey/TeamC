bd1209be7bd24f2671e681e183d425f544cb8ca8
Michael Banner <michael.banner@codecomputerlove.com>
2020-10-29 15:50:13 +0000

    Adding multiplication

--- calculator.js
+++ calculator.js
@@ -2,12 +2,26 @@ function sumNumbers(a, b){
 	return a + b;
 }
 
+function multiplyNumbers(a, b)
+{
+	return a * b;
+}
+
 module.exports = {
 	calculate : (problem) => {
 		var split = problem.split(" ");
 
-		var result = sumNumbers(parseInt(split[0]), parseInt(split[2]));
-		
-		return result;
+		var a = parseInt(split[0]);
+		var b = parseInt(split[2]);
+
+		var operator = split[1];
+
+		switch(operator)
+		{
+			case '+':
+				return sumNumbers(a, b);
+			case '*':
+				return multiplyNumbers(a, b);
+		}
 	}
 };
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,3 +3,7 @@ const calculator = require('../calculator.js');
 test('return 5', () => {
   expect(calculator.calculate("2 + 3")).toBe(5);
 });
+
+test('return 9', () => {
+  expect(calculator.calculate("3 * 3")).toBe(9);
+});
